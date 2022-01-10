40ba76a9765bb0841b1d7af6a44f435759a66975
vshivangi <Shivangi.Verma21@st.niituniversity.in>
2022-01-10 12:55:23 +0000

    java code added

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+    public static void main(String []args){
+        System.out.println("Hello");
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
